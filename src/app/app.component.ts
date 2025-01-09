b6b55e927fa67513aeb47660a5f2323af0caed5e
nikkinicholasromero <nikkinicholas.romero@gmail.com>
2025-01-09 22:11:19 +0800

    Added tailwind css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   `,
 })
 export class AppComponent {
